73c985cfedc192f2d33e5887c7269004e60828bc
Владимир Андреев <buy_sell@bk.ru>
2022-06-30 13:05:05 +0300

    “Start_commit”

---
+++ src/BonusMilesService.java
@@ -0,0 +1,7 @@
+public class BonusMilesService {
+    public int calculate(int price) {
+        int cost = 20; // 20 руб. за 1 миллю
+        int miles = price/cost; // кол-во целых миль
+        return miles;
+    }
+}
